01fd78d8da093a0c117806f69a3b1d782454e410
LauBGz <msg13@outlook.es>
2020-01-19 15:05:38 +0100

    ejercicios javascript

--- Contents/01-Front-end/03-JavaScript/01-VanillaJS/Bucles/index.js
+++ Contents/01-Front-end/03-JavaScript/01-VanillaJS/Bucles/index.js
@@ -288,66 +288,89 @@ let arrArticulos = [
     {nombre: "Avatar", tipo: "P", valoracion: "1"}
 ];
 
-document.write(`<h2>Series</h2>`);
+// document.write(`<h2>Series</h2>`);
 
-for (let i = 0; i < arrArticulos.length; i++) {
-    if (arrArticulos[i]["tipo"] === "S") {
-        if (arrArticulos[i]["nombre"] === "Breaking Bad") {
-            document.write(`<p>Nombre de la serie: <span style="color:green;">${arrArticulos[i]["nombre"]}</span>. Valoración: ${arrArticulos[i]["valoracion"]}.</p>`);
-        }else {
-            let serie = `<p>Nombre de la serie: ${arrArticulos[i]["nombre"]}. Valoración: ${arrArticulos[i]["valoracion"]}.</p>`;
-            document.write(serie);
-        }
-    }
-}
+// for (let i = 0; i < arrArticulos.length; i++) {
+//     if (arrArticulos[i]["tipo"] === "S") {
+//         if (arrArticulos[i]["nombre"] === "Breaking Bad") {
+//             document.write(`<p>Nombre de la serie: <span style="color:green;">${arrArticulos[i]["nombre"]}</span>. Valoración: ${arrArticulos[i]["valoracion"]}.</p>`);
+//         }else {
+//             let serie = `<p>Nombre de la serie: ${arrArticulos[i]["nombre"]}. Valoración: ${arrArticulos[i]["valoracion"]}.</p>`;
+//             document.write(serie);
+//         }
+//     }
+// }
 
-document.write(`<h2>Peliculas</h2>`);
+// document.write(`<h2>Peliculas</h2>`);
 
-let arrPelis = [];
+// let arrPelis = [];
 
-for (let i = 0; i < arrArticulos.length; i++) {
-    if (arrArticulos[i]["tipo"] === "P"){
-        if (arrArticulos[i]["nombre"] === "Pulp Fiction") {
-            document.write(`<p><span style="color:red;">${arrArticulos[i]["nombre"]}</span> ${arrArticulos[i]["valoracion"]}<i class="fa fa-star-o"></i></p>`);
-        }else {
-            let pelicula = `<p>${arrArticulos[i]["nombre"]} ${arrArticulos[i]["valoracion"]}<i class="fa fa-star-o"></i></p>`;
-            document.write(pelicula);
-        }
-    }
-}
-
-
-let numMaxPeli = 0;
-let numMaxSerie = 0;
-let i;
+// for (let i = 0; i < arrArticulos.length; i++) {
+//     if (arrArticulos[i]["tipo"] === "P"){
+//         if (arrArticulos[i]["nombre"] === "Pulp Fiction") {
+//             document.write(`<p><span style="color:red;">${arrArticulos[i]["nombre"]}</span> ${arrArticulos[i]["valoracion"]}<i class="fa fa-star-o"></i></p>`);
+//         }else {
+//             let pelicula = `<p>${arrArticulos[i]["nombre"]} ${arrArticulos[i]["valoracion"]}<i class="fa fa-star-o"></i></p>`;
+//             document.write(pelicula);
+//         }
+//     }
+// }
 
 
-for (i = 0; i < arrArticulos.length; i++) {
-    if (arrArticulos[i]["tipo"] === "P") {
-    let num = parseFloat(arrArticulos[i]["valoracion"]);
-        if (numMaxPeli < num){
-            numMaxPeli = num;
-        } 
-    }
-}
+// let numMaxPeli = 0;
+// let numMaxSerie = 0;
+// let i;
+
+
+// for (i = 0; i < arrArticulos.length; i++) {
+//     if (arrArticulos[i]["tipo"] === "P") {
+//     let num = parseFloat(arrArticulos[i]["valoracion"]);
+//         if (numMaxPeli < num){
+//             numMaxPeli = num;
+//             if (parseFloat(arrArticulos[i]["valoracion"]) === numMaxPeli) {
+//                 document.write(`<h3>Mejor película</h3>`+arrArticulos[i]["nombre"] );
+//             }
+//         } 
+//     }
+// }
  
 
+// for (i = 0; i < arrArticulos.length; i++) {
+//     if (arrArticulos[i]["tipo"] === "S") {
+//     let num = parseFloat(arrArticulos[i]["valoracion"]);
+//         if (numMaxSerie < num){
+//             numMaxSerie = num;
+//             if (parseFloat(arrArticulos[i]["valoracion"]) === numMaxSerie) {
+//                 document.write(`<h3>Mejor serie</h3>`+arrArticulos[i]["nombre"] );
+//             }
+//         }       
+//     }
+// }
 
-for (i = 0; i < arrArticulos.length; i++) {
-    if (arrArticulos[i]["tipo"] === "S") {
-    let num = parseFloat(arrArticulos[i]["valoracion"]);
-        if (numMaxSerie < num){
-            numMaxSerie = num;
-        } 
+//17 enero - Ejercicio 2
+
+ const articulos = arrArticulos;
+ let artHtml = [];
+ let artCsv = [];
+
+ console.log(articulos);
+
+ let strFormato = prompt("Formato de Exportación (HTML o CSV):");
+
+ if (strFormato === "HTML") {
+    for (let i = 0; i < articulos.length; i++){
+        artHtml.push(`<tr><td>`+articulos[i]["nombre"]+`</td><td>`+articulos[i]["tipo"]+`</td><td>`+articulos[i]["valoracion"]+`</td></tr>`); 
     }
+    document.write(`<table><tr><td>Artículo</td><td>Tipo</td><td>Valoración</td></tr>`+artHtml+`</table>`);
+} else {
+    for (let i = 0; i < articulos.length; i++){
+        artCsv.push(`<div>`+articulos[i]["nombre"]+`;`+articulos[i]["tipo"]+`;`+articulos[i]["valoracion"]+`;</div>`); 
+    }
+    document.write(`<div>Artículo;Tipo;Valoración</div>`+artCsv);
 }
 
 
-document.write(`<h3>Mejor serie</h3>`);
-document.write(`<h3>Mejor película</h3>`);
 
-
-//17 enero - Ejercicio 2
 
 
 
