d5a1dbd70962b8457f16c5f4a5bee859d86aa8f5
LauBGz <msg13@outlook.es>
2020-01-22 17:58:04 +0100

    ejercicios math

---
+++ Contents/01-Front-end/03-JavaScript/01-VanillaJS/Math/index.js
@@ -0,0 +1,34 @@
+//funcion que ordena al azar una serie de elementos
+
+// let arrAnimales = ["Alpaca","Ballena", "Cebra", "León", "Antílope", "Aguila", "Cabra", "Rata"];
+
+// function changeArray(array){
+//     let arrShuffle = [];
+//         for (let i = 0; i < array.length; i++){
+//             arrShuffle.push(array[Math.floor(Math.random() * array.length) + 1  ]);
+//         }
+//     return arrShuffle;
+// }
+
+
+// let arrAzar = changeArray(arrAnimales);
+// console.log(arrAzar);
+
+// let arrAnimales = ["Alpaca","Ballena", "Cebra", "León", "Antílope", "Aguila", "Cabra", "Rata"];
+
+// function shuffleArray(array) {
+//     let result = [];
+//     for (let i=0; i < array.length; i++){
+//         let elementoRandom = array[Math.floor(Math.random() * (array.length))];
+//         if (result.indexOf(elementoRandom) === -1){
+//             result.push(elementoRandom)
+//         } else {
+//             i--;
+//         }
+//     }
+//     return result;
+// }
+
+// let cambio = shuffleArray(arrAnimales);
+
+// console.log(cambio);
