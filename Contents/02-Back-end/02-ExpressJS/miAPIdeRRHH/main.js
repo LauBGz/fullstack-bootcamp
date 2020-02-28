d376bc65a30418df8e34f01cb4b451d0349fca66
LauBGz <msg13@outlook.es>
2020-02-28 12:36:19 +0100

    ejercicio finalizado

--- Contents/02-Back-end/02-ExpressJS/miAPIdeRRHH/main.js
+++ Contents/02-Back-end/02-ExpressJS/miAPIdeRRHH/main.js
@@ -22,13 +22,23 @@ servidor.get('/employees', (req, res) =>{
 })
 
 //Endpoint /employee/:indice(GET)
-servidor.get('/employee/:indice', (req, res) => {
+servidor.get('/employee/:id', (req, res) => {
   fs.readFile('users.json', (error, fileContents) => {
     if (error) throw error;
+    
     const datosUsuarios = JSON.parse(fileContents);
-    const usuario = datosUsuarios[req.params.indice];
-    res.send(usuario);
-    return;
+    
+    const idUsuario = req.params.id;
+
+    let result = datosUsuarios.find(obj => {
+      return(obj.id === idUsuario)
+    })
+
+    if (result === undefined) {
+      res.send({"message": "Usuario no existe."});
+    } else {
+      res.send(result);
+    }
   })
 })
 
@@ -40,12 +50,15 @@ servidor.post('/addEmployee', (req, res) =>{
 
         fs.readFile('users.json', (error, fileContents) => {
           if(error) throw error;      
-          const data = JSON.parse(fileContents);
-          
-          let positionID = data.length-1;
-          let id = data[positionID]["id"];
-          idUsuario = parseInt(id) + 1;
-      
+          const data = JSON.parse(fileContents)
+
+          let idUsuario = 0;
+          for (const user of data) {
+              if (user["id"] > idUsuario){
+                idUsuario = user["id"]
+              }
+              idUsuario++;
+            } 
           const userData = {
             "FirstName": req.body["FirstName"], 
             "SecondName": req.body["SecondName"], 
@@ -71,31 +84,34 @@ servidor.post('/addEmployee', (req, res) =>{
 servidor.put('/editEmployee', (req, res) =>{
   if( req.body["FirstName"] && req.body["SecondName"] && req.body["Departamento"] 
   && req.body["Salario"] && req.body["Salario"] && req.body["id"]){
-    //CONTENIDO IF
     fs.readFile('users.json', (error, fileContents) => {
       if(error) throw error;      
       const data = JSON.parse(fileContents);
-      //FOR PARA COMPARAR ID
-      for (let i = 0; i < data.length; i++) {
+      let posUsuarioEncontrado = -1;
+
+      for (i = 0; i < data.length; i++) {
         if (data[i]["id"] === req.body["id"]){
-          //Si existe usuario
-          const newUserData = {
-            "FirstName": req.body["FirstName"], 
-            "SecondName": req.body["SecondName"], 
-            "Departamento": req.body["Departamento"], 
-            "Salario": req.body["Salario"], 
-            "id": req.body["id"]
-            }
-          data.splice(i, 1, newUserData);
-          fs.writeFile('users.json', JSON.stringify(data), (error) => {
-            if (error) throw error;
-            res.send({"message": "Usuario modificado con éxito!"});
-            return;
-          })
-        }else{  
-          res.send({"message": "Usuario no encontrado."});
-          return;
+          posUsuarioEncontrado = i;
         }
+      }
+
+      if (posUsuarioEncontrado > -1){
+        const newUserData = {
+          "FirstName": req.body["FirstName"], 
+          "SecondName": req.body["SecondName"], 
+          "Departamento": req.body["Departamento"], 
+          "Salario": req.body["Salario"], 
+          "id": req.body["id"]
+          }
+        data.splice(posUsuarioEncontrado, 1, newUserData);
+        fs.writeFile('users.json', JSON.stringify(data), (error) => {
+          if (error) throw error;
+          res.send({"message": "Usuario modificado con éxito!"});
+          return;
+        })
+      }else{
+        res.send({"message": "Usuario no encontrado."});
+        return;
       }
     })
   }else{
@@ -105,22 +121,41 @@ servidor.put('/editEmployee', (req, res) =>{
 })
 
 //Endpoint /deleteEmployee/:indice(DELETE) 
-servidor.delete('/deleteEmployee/:indice', (req, res) =>{
+servidor.delete('/deleteEmployee/:id', (req, res) =>{
   fs.readFile('users.json', (error, fileContents) => {
     if (error) throw error;
+
     let datosUsuarios = JSON.parse(fileContents);
-    const idUsuario = req.params.indice;
-   
-    datosUsuarios.splice(idUsuario, 1);
 
-    fs.writeFile('users.json', JSON.stringify(datosUsuarios), (error) => {
-      if (error) throw error;
-      res.send({"message": "Usuario borrado con éxito!"});
+    const idUsuario = req.params.id;
+
+    let posArray;
+
+    let result = datosUsuarios.find(obj => {
+      return(obj.id === idUsuario);
+    })  
+
+    for (i = 0; i < datosUsuarios.length; i++) {
+      if(datosUsuarios[i] === result){
+        posArray = i;
+      }
+    }
+
+    datosUsuarios.splice(posArray, 1)
+
+    if(result === undefined){
+      res.send({"message": "Usuario no existe"});
       return;
-    })
-    })
+    } else {
+      fs.writeFile('users.json', JSON.stringify(datosUsuarios), (error) => {
+        if (error) throw error;
+        res.send({"message": "Usuario borrado con éxito!"});
+        return;
+      })
+    }
+  })
 });
-
+  
 
 //Configuración servidor
 servidor.listen(3000, () => {
